ddf9f35fa82ea925f4e7be71fb60c7aae1351eef
abdulrhmaanm <abdulrhmanmahmod77@icloud.com>
2025-08-26 02:59:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Layout from './Components/Layout'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import Posts from './Pages/Posts'
-import NotFound from './Pages/NotFound'
-import { createBrowserRouter, RouterProvider ,  } from 'react-router-dom'
-import AuthContextProvider from './context/AuthContext'
-import CounterContextProvider from './context/CounterContext'
-import ProtectedAuthRoutes from './ProtectRoutes/ProtectedAuthRoutes'
-import ProtectedRoutes from './ProtectRoutes/ProtectedRoutes';
-import PostDetails from './Pages/PostDetails'
-import { Profile } from './Pages/Profile'
+import { useState } from "react";
+import "./App.css";
+import Layout from "./Components/Layout";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Posts from "./Pages/Posts";
+import NotFound from "./Pages/NotFound";
+import PostDetails from "./Pages/PostDetails";
+import { Profile } from "./Pages/Profile";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthContextProvider from "./context/AuthContext";
+import CounterContextProvider from "./context/CounterContext";
+import ProtectedAuthRoutes from "./ProtectRoutes/ProtectedAuthRoutes";
+import ProtectedRoutes from "./ProtectRoutes/ProtectedRoutes";
 
 function App() {
-const route = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoutes>
-        <Layout />
-      </ProtectedRoutes>
-    ),
-    children: [
-      { index: true, element: <Posts /> },
-      { path: "posts", element: <Posts /> }
-    ]
-  },
-  {
-    element: <Layout />,
-    children: [
-
-      
-        {
-        path: "/profile",
-        element: (
-          <ProtectedRoutes>
-            <Profile />
-          </ProtectedRoutes>
-        )
-      },
-        {
-        path: "/posts/postdetails/:id",
-        element: (
-          <ProtectedRoutes>
-            <PostDetails />
-          </ProtectedRoutes>
-        )
-      },
-      {
-        path: "/login",
-        element: (
-          <ProtectedAuthRoutes>
-            <Login />
-          </ProtectedAuthRoutes>
-        )
-      },
-      {
-        path: "/register",
-        element: (
-          <ProtectedAuthRoutes>
-            <Register />
-          </ProtectedAuthRoutes>
-        )
-      }
-    ]
-  },
-  { path: "*", element: <NotFound /> }
-]);
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoutes>
+          <Layout />
+        </ProtectedRoutes>
+      ),
+      children: [
+        { index: true, element: <Posts /> },
+        { path: "posts", element: <Posts /> },
+        { path: "posts/postdetails/:id", element: <PostDetails /> },
+        { path: "profile", element: <Profile /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: (
+        <ProtectedAuthRoutes>
+          <Login />
+        </ProtectedAuthRoutes>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <ProtectedAuthRoutes>
+          <Register />
+        </ProtectedAuthRoutes>
+      ),
+    },
+    { path: "*", element: <NotFound /> },
+  ]);
 
   return (
     <AuthContextProvider>
       <CounterContextProvider>
-        <RouterProvider router={route} />
+        <RouterProvider router={router} />
       </CounterContextProvider>
     </AuthContextProvider>
   );
